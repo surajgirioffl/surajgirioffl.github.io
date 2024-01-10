db08ed54f7072e507f14bbeabfe496b2d782e539
Suraj Kumar Giri <surajkumargiri2002@gmail.com>
2024-01-11 00:00:48 +0530

    Feat: Implement `emailjs` to send contact form data
    Implement 'emailjs' to send the contact form data to the
    developer email.
    
    Signed-off-by: @surajgirioffl

---
+++ assets/js/contact.js
@@ -0,0 +1,73 @@
+let publicKey = 'vwR4AXWqci-2xv8EA'
+emailjs.init(publicKey)
+
+const validateFormData = () => {
+    const name = document.getElementById('name')
+    if (!name.value) {
+        alert("Name is required.")
+        return false;
+    }
+
+    const email = document.getElementById('email')
+    if (!email.value) {
+        alert("Email is required.")
+        return false;
+    }
+
+    const subject = document.getElementById('subject')
+    if (!subject.value) {
+        alert("Subject is required.")
+        return false;
+    }
+
+    const message = document.querySelector("textarea[name='message']")
+    if (!message.value) {
+        alert("Message is required.")
+        return false;
+    }
+    return true;
+}
+
+const fetchFormData = () => {
+    const name = document.getElementById('name').value
+    const email = document.getElementById('email').value
+    const subject = document.getElementById('subject').value
+    const message = document.querySelector("textarea[name='message']").value
+    return {
+        from_name: name,
+        from_email: email,
+        reply_to: email,
+        to_name: 'Suraj Kumar Giri',
+        subject: subject,
+        message: message,
+        date_time: new Date().toString()
+    }
+}
+
+const contactForm = document.getElementsByTagName('form')[0]
+contactForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+}
+)
+
+document.querySelector('button[type="submit"]').addEventListener('click', () => {
+    if (validateFormData()) {
+        emailjs.send('service_gee98ws', 'template_oiozxsa', templateParams = fetchFormData(), publicKey)
+            .then((response) => {
+                if (response.status == 200) {
+                    document.getElementById('custom-error-message').style.display = 'none';
+                    console.log(document.getElementById('custom-error-message'))
+                    document.getElementsByClassName('sent-message')[0].style.display = 'block';
+                }
+                else {
+                    document.getElementById('custom-error-message').style.display = 'block';
+                    document.getElementById('custom-error-message').innerHTML = 'Error: Something went wrong while sending request to the server.';
+                }
+            }
+            )
+    }
+    else {
+        document.getElementById('custom-error-message').style.display = 'block';
+        document.getElementById('custom-error-message').innerHTML = 'Error: Please fill all the required fields.';
+    }
+})
